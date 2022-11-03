7e14cc1d4de0d2b354a112ba85de3319f9e161a9
Arbazbms <ArbazAhmed.cs19@bmsce.ac.in>
2022-11-03 14:22:09 +0530

    few dao changes added

--- src/main/java/com/fidelity/smallchange/controller/PreferenceController.java
+++ src/main/java/com/fidelity/smallchange/controller/PreferenceController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ServerErrorException;
 import org.springframework.web.server.ServerWebInputException;
 
 import com.fidelity.smallchange.integration.PreferenceDao;
+import com.fidelity.smallchange.integration.PreferenceDao;
 import com.fidelity.smallchange.models.Preference;
 
 
--- src/main/java/com/fidelity/smallchange/integration/PreferenceDao.java
+++ src/main/java/com/fidelity/smallchange/integration/PreferenceDao.java
@@ -1,9 +1,26 @@
 package com.fidelity.smallchange.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.fidelity.smallchange.integration.mapper.PreferenceMapper;
 import com.fidelity.smallchange.models.Preference;
 
-public interface PreferenceDao {
-	
-	Preference getPreference(String id);
-	int insertPreference(Preference preference);
+@Repository
+public class PreferenceDao {
+
+	@Autowired
+	private PreferenceMapper mapper;
+
+	public Preference getPreference(String id) {
+		// TODO Auto-generated method stub
+		return mapper.getPreference(id);
+	}
+
+
+	public int insertPreference(Preference preference) {
+		// TODO Auto-generated method stub
+		return mapper.insertPreference(preference);
+	}
+
 }
