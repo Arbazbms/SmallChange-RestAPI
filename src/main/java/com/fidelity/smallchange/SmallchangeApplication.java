a9e5fc44b25ce35380a1296792d64a5e35c9b4a1
MVRanjitha <ranjithamva@gmail.com>
2022-11-03 14:38:11 +0530

    client

--- src/main/java/com/fidelity/smallchange/SmallchangeApplication.java
+++ src/main/java/com/fidelity/smallchange/SmallchangeApplication.java
@@ -1,9 +1,16 @@
 package com.fidelity.smallchange;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.fidelity.smallchange.integration",
+	"com.fidelity.smallchange.integration.mapper",
+	"com.fidelity.smallchange.controller", 
+	"com.fidelity.smallchange.service"
+})
+@MapperScan(basePackages="com.fidelity.smallchange.integration.mapper")  
 public class SmallchangeApplication {
 
 	public static void main(String[] args) {
